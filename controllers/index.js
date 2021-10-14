f14ec294f08667eecae0a6eb2e99f680301201b0
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-10-15 00:39:47 +0530

    Mongoose schema added for 'developer' and 'buyer', basic user data validation created

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,24 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+const BuyerModel = require("../models/buyer");
+const DeveloperModel = require("../models/developer");
+const { checkUserExist } = require("../helpers");
+
 module.exports = {
-    sayHi: (req, res, next)=> {
+  sendConfirmEmailToken: (req, res, next) => {
+    let { email, password, userType } = req.body;
+
+    checkUserExist(email, userType)
+      .then(response => {
+        let { userExist, message } = response;
+        if (userExist) next(new ErrorResponse(409, message));
+        // Need to add verification email sending with "nodemailer"
         res.status(200).json({
-            success: true,
-            message: "Let's Build Together"
-        })
-    },
-}
+          success: true,
+          message: "Verify email successfully send to " + email,
+        });
+      })
+      .catch(err => {
+        next(new ErrorResponse(401, err.message));
+      });
+  },
+};
--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,47 @@
+const BuyerModel = require("../models/buyer");
+const DeveloperModel = require("../models/developer");
+
 module.exports = {
-  foo: (bar1, bar2) => {
-    // some code here
-  },
+  checkUserExist: (email, userType) =>
+    new Promise((resolve, reject) => {
+      switch (userType) {
+        case "developer":
+          DeveloperModel.findOne({ email: email }).then(developerExist => {
+            if (Boolean(developerExist))
+              resolve({
+                userExist: true,
+                message: "This developer email is already exist in developers",
+              });
+            else
+              resolve({
+                userExist: false,
+                message: "This developer doesn't exist yet",
+              });
+          });
+          break;
+        case "buyer":
+          BuyerModel.findOne({ email: email }).then(buyerExist => {
+            if (Boolean(buyerExist))
+              resolve({
+                userExist: true,
+                message: "This buyer email is already exist in buyers",
+              });
+            else
+              resolve({
+                userExist: false,
+                message: "This buyer doesn't exist yet",
+              });
+          });
+          break;
+        default:
+          reject({
+            message: "This user type isn't valid",
+          });
+      }
+    }),
+  createToken: userData =>
+    new Promise((resolve, reject) => {
+      // This isn't finished yet
+      let { email, password } = userData;
+    }),
 };
