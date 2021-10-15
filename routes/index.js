5b58a9fdee78a15c0e9752a1cc704c2d65e0e134
shamilkotta <shamilkotta99@gmail.com>
2021-10-15 18:57:13 +0530

    confirm-email router created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { sendConfirmEmailToken } = require("../controllers");
+const { sendConfirmEmailToken, createUser } = require("../controllers");
+const { validationResults, validateToken } = require("../middlewares/validations");
 const { validateBasicUser } = require("../middlewares/validations/userValidation");
 
-router.post("/create-user", validateBasicUser, sendConfirmEmailToken);
+router.post("/create-user", validateBasicUser, validationResults, sendConfirmEmailToken);
+router.post("/confirm-email", validateToken, validationResults, createUser)
 
 module.exports = router;
