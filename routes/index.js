f139ea3680f91d36e5ba0a55cf5f74875923f8d1
shamilkotta <shamilkotta99@gmail.com>
2021-10-17 20:54:12 +0530

    login route added

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { sendConfirmEmailToken, saveUser } = require("../controllers");
+const { sendConfirmEmailToken, saveUser, sendLoginToken } = require("../controllers");
 const { validationResults, validateToken } = require("../middlewares/validations");
 const { validateBasicUser } = require("../middlewares/validations/userValidation");
 
 router.post("/create-user", validateBasicUser, validationResults, sendConfirmEmailToken);
-router.post("/confirm-email", validateToken, validationResults, saveUser)
+router.post("/confirm-email", validateToken, validationResults, saveUser);
+router.post("/login", validateBasicUser, validationResults, sendLoginToken)
 
 module.exports = router;
