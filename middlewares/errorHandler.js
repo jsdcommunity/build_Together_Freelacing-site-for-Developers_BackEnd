a3d3145aac4b91a67518c8bfb0b3136761202939
shamilkotta <shamilkotta99@gmail.com>
2021-10-19 23:59:15 +0530

    error handler updated

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,15 +1,15 @@
 module.exports = (err, req, res, next)=> {
     let {statusCode = 500, message = "Internal Server Error"} = err;
 
-    console.log(err)
+    console.log(err);
 
-    if(statusCode == 500){
-        message = "Internal Server Error"
-    }
+    if (statusCode == 500) message = "Internal Server Error";
+    if (statusCode == 404 && message == "Internal Server Error") message = "Not found";
+    if (statusCode = 401 && message == "Internal Server Error") message = "Unauthorized";
 
     // sending error response
     res.status(statusCode).json({
         success: false,
         message
-    })
+    });
 }
