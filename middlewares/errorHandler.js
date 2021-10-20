71b5ce23d9fd93e6a13956cc63513dc101925931
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-10-20 17:46:39 +0530

    Removed some unnecessary code blocks from `errorHandler.js`

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,15 +1,13 @@
-module.exports = (err, req, res, next)=> {
-    let {statusCode = 500, message = "Internal Server Error"} = err;
+module.exports = (err, req, res, next) => {
+  let { statusCode = 500, message = "Internal Server Error" } = err;
 
-    console.log(err);
+  console.log(err);
 
-    if (statusCode == 500) message = "Internal Server Error";
-    if (statusCode == 404 && message == "Internal Server Error") message = "Not found";
-    if (statusCode = 401 && message == "Internal Server Error") message = "Unauthorized";
+  if (statusCode == 500) message = "Internal Server Error";
 
-    // sending error response
-    res.status(statusCode).json({
-        success: false,
-        message
-    });
-}
+  // sending error response
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
