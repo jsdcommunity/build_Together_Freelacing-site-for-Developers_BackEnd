806de6b9341a3f8c4b8550f662e04485d976e97a
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-10-20 17:56:04 +0530

    Added comments in `routes/index.js`

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,19 @@ const {
     saveUser,
     sendLoginToken,
     sendResetPasswordToken,
-    resetPassword
+  resetPassword,
 } = require("../controllers");
 const { validationResults, validateToken } = require("../middlewares/validations");
 const { validateBasicUser, validateEmail, validatePassword } = require("../middlewares/validations/userValidation");
 
+// Sign up user
 router.post("/create-user", validateBasicUser, validationResults, sendConfirmEmailToken);
 router.post("/confirm-email", validateToken, validationResults, saveUser);
 
+// Login user
 router.post("/login-user", validateBasicUser, validationResults, sendLoginToken);
 
+// Reset password feature in login section of user
 router.post("/forgot-password", validateEmail, validationResults, sendResetPasswordToken);
 router.post("/reset-password", validatePassword, validationResults, resetPassword);
 
