12da50824cc14c4289745e0aae13fafea942b750
shamilkotta <shamilkotta99@gmail.com>
2021-10-26 00:05:33 +0530

    added functionality for retrieving valid data

--- middlewares/validations/index.js
+++ middlewares/validations/index.js
@@ -1,10 +1,12 @@
-const { check, validationResult } = require("express-validator");
+const { check, validationResult, matchedData } = require("express-validator");
 const ErrorResponse = require("../../utils/ErrorResponse");
 
 module.exports = {
    validationResults: (req, res, next) => {
       const error = validationResult(req);
-      if (!error.isEmpty()) next(new ErrorResponse(401, error.array()[0].msg));
+      if (!error.isEmpty()) next(new ErrorResponse(400, error.array()[0].msg));
+      data = matchedData(req, {onlyValidData: true, includeOptionals: false});
+      req.validData = data;
       next();
    },
 
