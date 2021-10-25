955adb81cd78941983deed11b40ee6d40ea197e5
shamilkotta <shamilkotta99@gmail.com>
2021-10-26 00:02:02 +0530

    created authorization middleware

---
+++ middlewares/index.js
@@ -0,0 +1,28 @@
+const { verifyToken } = require("../helpers");
+const ErrorResponse = require("../utils/ErrorResponse");
+
+module.exports = {
+    getUserAccess: async (req, res, next) => {
+        const token = req.headers.authorization;
+        let userData;
+
+        // verifying token
+        try {
+            userData = await verifyToken(token);
+        } catch (err) {
+            if (err.name == "TokenExpiredError")
+                return next(
+                new ErrorResponse(401, "Unauthorized Please login again")
+                ); //error from token verification
+            if (err.name == "JsonWebTokenError")
+                return next(
+                new ErrorResponse(401, "Invalid login credentils")
+                ); //error from token verification
+            return next(err);
+        }
+        req.user = userData;
+        return next();
+    },
+
+    getAdminAccess: (req, res, next) => next(),
+}
