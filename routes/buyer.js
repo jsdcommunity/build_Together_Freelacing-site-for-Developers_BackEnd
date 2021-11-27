3660bd9420e4a3eb45d316d7b72b231d88911d56
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-11-27 16:13:06 +0530

    Router GET `buyer/get-jobs` : get created job requirements by an individual buyer

--- routes/buyer.js
+++ routes/buyer.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { saveJob } = require("../controllers/buyerControllers");
+const { saveJob, getJobs } = require("../controllers/buyerControllers");
 const validateJobFields = require("../middlewares/validations/jobValidation");
 const { validationResults } = require("../middlewares/validations");
 
 // Create a job requirement
 router.post("/create-job", validateJobFields, validationResults, saveJob);
+
+// Get created jobs
+router.get("/get-jobs", getJobs);
 
 module.exports = router;
